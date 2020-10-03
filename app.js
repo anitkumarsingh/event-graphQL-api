98337abb7342de0b4a9132e0508699105bbafd01
Anit Kumar <anikumarsingh99@gmail.com>
2020-10-03 13:21:45 +0530

    added basic graphql api

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 const PORT = process.env.PORT || 4000;
 const app = express();
+const events = ["Coding", "Playing", "Cooking", "Sleeping"];
 
 app.use(bodyParser.json());
-app.get("/", (req, res, next) => {
-  res.send("Hello world!");
-});
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+    type rootQuery {
+        events:[String!]!
+    }
+    type rootMutation {
+        createEvent(name:String):String
+    }
+      schema {
+          query:rootQuery
+          mutation:rootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        events.push(eventName);
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, () => console.log(`server is running ${PORT}`));

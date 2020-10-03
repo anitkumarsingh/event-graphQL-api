0377a9dc8133b1b34309a35d79f9753f3f933c08
Anit Kumar <anikumarsingh99@gmail.com>
2020-10-03 14:19:29 +0530

    added complex data type

--- app.js
+++ app.js
@@ -4,18 +4,30 @@ const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 const PORT = process.env.PORT || 4000;
 const app = express();
-const events = ["Coding", "Playing", "Cooking", "Sleeping"];
+const events = [];
 
 app.use(bodyParser.json());
 app.use(
   "/graphql",
   graphqlHttp({
     schema: buildSchema(`
+    type Event {
+        _id:ID!
+         name:String!
+         description:String!
+         price:Float!
+         createdAt:String
+    }
+    input EventInput {
+       name:String!
+       description:String!
+       price:Float!
+    }
     type rootQuery {
-        events:[String!]!
+        events:[Event!]!
     }
     type rootMutation {
-        createEvent(name:String):String
+        createEvent(eventInput:EventInput):Event
     }
       schema {
           query:rootQuery
@@ -27,9 +39,15 @@ app.use(
         return events;
       },
       createEvent: (args) => {
-        const eventName = args.name;
-        events.push(eventName);
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          name: args.eventInput.name,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          createdAt: new Date().toISOString(),
+        };
+        events.push(event);
+        return event;
       },
     },
     graphiql: true,
